6fa38d083d2281f94201fce50bac808ef16d24aa
Fitriakmaliah <114641149+Fitriakmaliah@users.noreply.github.com>
2022-10-07 17:20:05 +0700

    Add files via upload
    saya sudah mengerjakan tugas praktikum 1

---
+++ tugas praktikum 1_tugas 1.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    //TUGAS 1
+    string namalengkap = "Fitri Akmaliah";
+    string npm = "2210631170020";
+    int umur = 17;
+    string kelas = "1C Informatika";
+    string hobi = "Membaca AU";
+    string cita = "Dosen";
+
+    cout << "Nama Lengkap   :" << namalengkap << endl;
+    cout << "NPM            :" << npm << endl;
+    cout << "Umur           :" << umur << endl;
+    cout << "Kelas          :" << kelas << endl;
+    cout << "Hobi           :" << hobi << endl;
+    cout << "Cita           :" << cita << endl;
+
+
+
+
+return 0;
+}
